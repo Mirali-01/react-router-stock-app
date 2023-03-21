ca6620a56f3dde947774298d9905023f38331a2c
Mir <mir.m.ali01@gmail.com>
2023-03-21 17:38:30 -0400

    stock app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Stock from "./components/Stock";
+import Dashboard from "./components/Dashboard";
+import Nav from "./components/Nav";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+        <Route path="/stocks" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
